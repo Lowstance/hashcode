949c8bfd0b6fa14ed92b94ebc99cdfa412477a4f
Stefanos <stefgiannoulas@gmail.com>
2021-02-08 13:56:31 +0200

    added score function and automation

--- practice_round.py
+++ practice_round.py
@@ -2,15 +2,17 @@ import random
 import string
 from collections import Counter
 
-file_name = 'e_many_teams'
-w1 = 300
+file_name = 'd_many_pizzas'
+w1 = 0
 w2 = 1
-max_size = 1
+max_size = 0
+max_freq = 0
+
 
 #hello
 #parser IT WORKS
 
-def parser(file_name):
+def parser():
     global max_size
     file1 = open(file_name+'.in', 'r')
     first_line = file1.readline()
@@ -21,6 +23,7 @@ def parser(file_name):
         if int(token_line[0]) > max_size:
             max_size = int(token_line[0])
         info_array.append(token_line[2:-1].split(" "))
+
     file1.close()
     return [int(i) for i in first_line] , info_array
 
@@ -31,15 +34,14 @@ def write(final_indexes):
     file2.close()
 
 def evaluate(item, population_list):
-    frequency = 1 / sum([population_list.get(i) for i in item])
-    #print("freq" + str(frequency*w1))
+    frequency = sum([(max_freq-population_list.get(i))/max_freq for i in item])/len(item)
     size = len(item)/max_size
-    #print("size" + str(size*w2))
-    return frequency*w1 + size*w2
+    return (frequency*w1 + size*w2)/2
 
 def potatoMain(length, info_array):
     population_list = Counter(row for item in info_array for row in item) 
-
+    global max_freq
+    max_freq = max(population_list.values())
     score_list = []
     for i in range(0, length[0]):
         score_list.append([evaluate(info_array[i], population_list), i])
@@ -79,7 +81,58 @@ def create_result(final_indexes, length):
     result_array.insert(0, str(len(result_array)) + "\n")
     return result_array
 
+
+def score():
+    file1 = open(file_name+'_results.in', 'r')
+    file2 = open(file_name+'.in', 'r')
+    first_line = file1.readline()
+    result = []
+    for i in range(0 , int (first_line[:-1])):
+        token_line = file1.readline()
+        result.append(token_line[2:-1].split(" "))
+    #print(result)
+    first_line1 = file2.readline()
+    info_array = []
+    first_line1 = first_line1[:-1].split(" ")
+    for i in range(0 , int (first_line1[0])):
+        token_line = file2.readline()
+        info_array.append(token_line[2:-1].split(" "))
+
+
+    score = [0] * int(first_line[:-1])
+    for i in range(0, int(first_line[:-1])):
+        score[i] = len(list(filter(None, set([item for j in result[i] for item in info_array[int(j)]]))))
+        score[i] = score[i]*score[i]
+
+    file1.close()
+    file2.close()
+    return sum(score)
+
+def automatic():
+    max_w1 = 0
+    max_score = 0
+    global w1
+    global w2
+    w1 = 0
+    w2 = 1
+    for i in range(0,10):
+        [length, info] = parser()
+        final_indexes = potatoMain(length, info)
+        write(create_result(final_indexes, length))
+        cur_score = score()
+        if(cur_score>max_score):
+            max_score = cur_score
+            max_w1 = w1
+
+        w1 += 0.1
+        w2 -= 0.1
+        return max_score, max_w1
+        
+
+
 if __name__ == '__main__':
-    [length, info] = parser(file_name)
+    [length, info] = parser()
     final_indexes = potatoMain(length, info)
     write(create_result(final_indexes, length))
+    score()
+    #automatic()
