af1dc9be297511f7728d7878420b5613bc741ffe
Stefanos <stefgiannoulas@gmail.com>
2021-02-12 18:49:27 +0200

    Added optimization

--- hashcode2020/hash_2020.py
+++ hashcode2020/hash_2020.py
@@ -1,17 +1,14 @@
-
-file_name = 'hashcode2020/a_example'
+file_name = 'hashcode2020/f_libraries_of_the_world'
 
 def parser():
     file1 = open(file_name+'.txt', 'r')
     data = file1.readlines()
-    first_line = data[0].split(" ")
-    book_scores = data[1].split(" ")
+    first_line = data[0][:-1].split(" ")
+    book_scores = data[1][:-1].split(" ")
     lib_array = [i[:-1].split(" ") for i in data[2::2]]
     lib_book = [i[:-1].split(" ") for i in data[3::2]]
     file1.close()
-    print(lib_array)
-    print(lib_book)
-    return lib_book
+    return first_line, book_scores, lib_array, lib_book
 
 
 def fill_array(lib_book):
@@ -20,30 +17,21 @@ def fill_array(lib_book):
         indexes.append([i, len(lib_book[i])])
         indexes.append(lib_book[i])
 
-    print(indexes)
     return indexes
 
-def make_array(indexes):
-    final_array = []
-    final_array.append(f"{int(len(indexes)/2)}\n")
-    for i in range(0, len(indexes), 2):
-        line1 = f"{str(indexes[i][0])}  {str(indexes[i][1])}\n"
-        final_array.append(line1)
-        line2 = ''
-        for j in range(len(indexes[i+1])):
-            line2 += indexes[i+1][j] + ' '
+def sortBooks(lib_book, book_scores): 
+    for k in range(len(lib_book)):
+        n = len(lib_book[k]) 
+        y = [book_scores[int(l)] for l in lib_book[k]]
+        lib_book[k] = [x for _,x in sorted(zip(y, lib_book[k]), reverse=True)]
+    return lib_book
 
-        final_array.append(line2[:-1]+'\n')
+def sortLibraries(lib_array, lib_book): 
+    n = len(lib_array)
+    y = [k[2] for k in lib_array]
+    Z = [x for _,x in sorted(zip(y,zip(lib_array, lib_book)))]
+    return Z
 
-    return final_array
-
-def score():
-    file1 = open(file_name+'_results.txt', 'r')
-    file2 = open(file_name+'.txt', 'r')
-    first_line = file1.readline()
-    result = []
-    for i 0->day:
-        
 
 def write(final_indexes):
     file2 = open(file_name+'_results.txt', 'w')
@@ -51,6 +39,6 @@ def write(final_indexes):
     file2.close()
 
 if __name__ == '__main__':
-    write(make_array(fill_array(parser())))
-
+    first_line, book_scores, lib_array, lib_book = parser()
+    sortLibraries(lib_array, sortBooks(lib_book, book_scores))
 
